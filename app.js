945229abcebb622bb8d574e6c0eb7182c8f15317
Aziz benhmida <benhmida.mohamedaziz@esprit.tn>
2023-05-20 16:29:10 +0100

    about and help html pages

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const path = require("path");
 const geocode = require("./utils/geocode.js");
 const forecast = require("./utils/forecast.js");
 
 const app = express();
 
+console.log(__dirname);
+console.log(path.join(__dirname,'./public'));
+
+const publicDir=path.join(__dirname,'./public')
+
+app.use(express.static(publicDir))
 app.get("", (req, res) => {
   res.send("<h1> Weather</h1>");
 });
@@ -20,21 +27,6 @@ app.get("/weather", (req, res) => {
 app.listen(3000, () => {
   console.log("server is up on port 3000");
 });
-
-// const url =
-//   "http://api.weatherapi.com/v1/current.json?key=a1e87be597284d5f921184854231905&q=Tunis";
-// request({ url: url, json: true }, (error, response) => {
-//   if (error) {
-//     console.log("Unable to connect to the server!");
-//   } else if (response.body.error) {
-//     console.log("Unable to find location!");
-//   } else {
-//     console.log(response.body.current);
-//     console.log(
-//       `its currently ${response.body.current.temp_c} degree c. it's ${response.body.current.condition.text} outside `
-//     );
-//   }
-// });
 
 const arg = process.argv[2];
 
