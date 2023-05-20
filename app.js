72197d122b1135418d5125043c4c45e1ce69a4da
Aziz benhmida <benhmida.mohamedaziz@esprit.tn>
2023-05-20 15:54:16 +0100

    set up the view

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const request = require("request");
-
+const express = require("express");
 const geocode = require("./utils/geocode.js");
 const forecast = require("./utils/forecast.js");
+
+const app = express();
+
+app.get("", (req, res) => {
+  res.send("<h1> Weather</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1> About </h1>");
+});
+app.get("/weather", (req, res) => {
+  res.send({
+    forecast: "It is snowing",
+    location: "Tunis",
+  });
+});
+app.listen(3000, () => {
+  console.log("server is up on port 3000");
+});
+
 // const url =
 //   "http://api.weatherapi.com/v1/current.json?key=a1e87be597284d5f921184854231905&q=Tunis";
 // request({ url: url, json: true }, (error, response) => {
@@ -17,16 +36,15 @@ const forecast = require("./utils/forecast.js");
 //   }
 // });
 
-const arg=process.argv[2];
+const arg = process.argv[2];
 
-if(!arg){
-    console.log("Please add a city");
-}
-else{
-geocode(arg, (error, data) => {
-    if(error){
-        return console.log(error)
+if (!arg) {
+  console.log("Please add a city");
+} else {
+  geocode(arg, (error, data) => {
+    if (error) {
+      return console.log(error);
     }
-  forecast(error, data);
-});
+    forecast(error, data);
+  });
 }
